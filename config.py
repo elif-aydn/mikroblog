73c6bb662107ccf44c6ab0df7c9c40969625b582
elif-aydn <elifaydin8154@gmail.com>
2026-07-28 14:42:41 +0300

    feat: configure SQLite database
    Refs #1

--- config.py
+++ config.py
@@ -4,5 +4,15 @@
 import os
 
 
+basedir = os.path.abspath(os.path.dirname(__file__)) #Projenin kök dizininin tam adresini bulur.
+
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    SQLALCHEMY_DATABASE_URI = (
+        os.environ.get('DATABASE_URL') # DATABASE_URL: İleride farklı bir veritabanı kullanmak istersek ortam değişkeninden adres almamızı sağ
+        or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
