35a9f259c0597c27c605c264ea0c1904dd96c059
elif-aydn <elifaydin8154@gmail.com>
2026-07-29 09:57:59 +0300

    feat: add secure password handling

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(db.Model):
@@ -29,7 +30,12 @@ class User(db.Model):
     posts: so.WriteOnlyMapped["Post"] = so.relationship(
         back_populates="author"
     )
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
