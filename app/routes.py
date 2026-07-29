52da544ff250e71382c690e47d0cafa4c6a9ffaf
elif-aydn <elifaydin8154@gmail.com>
2026-07-29 11:58:22 +0300

    feat: add profile editor
    Refs #4

--- app/routes.py
+++ app/routes.py
@@ -16,7 +16,7 @@ from flask_login import (
     login_user,
     logout_user,
 )
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 
 @app.before_request
 def before_request():
@@ -141,4 +141,28 @@ def user(username):
         'user.html',
         user=user,
         posts=posts
+    )
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+
+        db.session.commit()
+
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit_profile'))
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+
+    return render_template(
+        'edit_profile.html',
+        title='Edit Profile',
+        form=form
     )
