c892e60227268eb2e28d3fc74529fb9c64308e13
elif-aydn <elifaydin8154@gmail.com>
2026-07-29 10:36:31 +0300

    feat: add user registration
    Refs #3

--- app/models.py
+++ app/models.py
@@ -33,6 +33,9 @@ class User(db.Model, UserMixin):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
     
