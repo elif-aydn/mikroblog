6d9c5702b85be52a234ce32dc8462debc0d2e821
elif-aydn <elifaydin8154@gmail.com>
2026-07-30 11:46:57 +0300

    Profil sayfalarinda takip ve takipci sayilarinin gorunmesi saglandi

--- app/routes.py
+++ app/routes.py
@@ -17,6 +17,8 @@ from flask_login import (
     logout_user,
 )
 from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import EmptyForm
+
 
 @app.before_request
 def before_request():
@@ -125,6 +127,8 @@ def user(username):
     user = db.first_or_404(
         sa.select(User).where(User.username == username)
     )
+    form = EmptyForm()
+    return render_template("user.html", user=user, form=form)
 
     posts = [
         {
@@ -168,6 +172,47 @@ def edit_profile():
         title='Edit Profile',
         form=form
     )
+
+@app.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == username))
+        if user is None:
+            flash(f'User {username} not found.')
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot follow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash(f'You are following {username}!')
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+    if form.validate_on_submit():
+        user = db.session.scalar(
+            sa.select(User).where(User.username == username))
+        if user is None:
+            flash(f'User {username} not found.')
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You cannot unfollow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.unfollow(user)
+        db.session.commit()
+        flash(f'You are not following {username}.')
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
+
 
 # @app.route("/senol")
 # def senol():
