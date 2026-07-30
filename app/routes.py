dc437d0f537af5b2009132d80eec2c185e2c8868
elif-aydn <elifaydin8154@gmail.com>
2026-07-30 14:40:23 +0300

    Kullanici erisimi daha kolay olmasi icin Explarer sayfasi eklendi

--- app/routes.py
+++ app/routes.py
@@ -133,22 +133,6 @@ def user(username):
     form = EmptyForm()
     return render_template("user.html", user=user, form=form)
 
-    posts = [
-        {
-            'author': user,
-            'body': 'Test post #1'
-        },
-        {
-            'author': user,
-            'body': 'Test post #2'
-        }
-    ]
-
-    return render_template(
-        'user.html',
-        user=user,
-        posts=posts
-    )
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
@@ -216,6 +200,12 @@ def unfollow(username):
     else:
         return redirect(url_for('index'))
 
+@app.route('/explore')
+@login_required
+def explore():
+    query = sa.select(Post).order_by(Post.timestamp.desc())
+    posts = db.session.scalars(query).all()
+    return render_template('index.html', title='Explore', posts=posts)
 
 # @app.route("/senol")
 # def senol():
