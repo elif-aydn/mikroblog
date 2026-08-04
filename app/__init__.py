83f870a5cf54ae2ba2ce664144f2097ca3ef3382
elif-aydn <elifaydin8154@gmail.com>
2026-08-04 16:40:40 +0300

    refactor: move error handling into blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -25,6 +25,8 @@ login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 moment = Moment(app)
 babel = Babel(app, locale_selector=get_locale)
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -55,4 +57,4 @@ if not app.debug:
     app.logger.info('Microblog startup')
 
 
-from app import routes, models, errors
+from app import routes, models
